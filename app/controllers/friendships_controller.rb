a74b63c05a93ad851422418263e4b6bbdaa47753
Alertzero <faceroxz@gmail.com>
2021-02-11 14:24:20 +0700

    Finalize app

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,5 +1,15 @@
 class FriendshipsController < ApplicationController
   def create
+    friend = User.find(params[:friend])
+    current_user.friendships.build(friend_id: friend.id)
+    if current_user.save 
+    
+      flash[:notice] = "Added friend"
+    else
+      flash[:alert] = "There was something wrong"
+
+    end
+    redirect_to my_friends_path
   end
 
   def destroy
